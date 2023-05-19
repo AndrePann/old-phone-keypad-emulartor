29d1f280c54b27b0c30384b404a40ca17a52e95a
a_albrecht <a.albrecht@home-albrecht.de>
2023-05-19 10:07:26 +0200

    create structure for OldPhone implementation

--- OldPhone.Keypand/OldPhone.Keypad.Emulator/OldPhone.cs
+++ OldPhone.Keypand/OldPhone.Keypad.Emulator/OldPhone.cs
@@ -6,7 +6,55 @@ using System.Threading.Tasks;
 
 namespace OldPhone.Keypad.Emulator
 {
-    public class OldPhone
+    /// <summary>
+    /// OldPhone implements the logik to emulate key pad events to pars input to result output.
+    /// </summary>
+    public static class OldPhone
     {
+        private static Dictionary<string, string> keyPadDictionary;
+
+        public static string OldPhonePad(string input)
+        {
+            return input;
+        }
+
+        private static void InitKeyPadDictionary()
+        {
+
+        }
+
+        private static bool ValidationInput(string input, string regExRule, out string validationResult)
+        {
+            bool isValid = false;
+
+            validationResult = "";
+
+            return isValid;
+        }
+
+        private static void ProcessInputKey()
+        {
+            
+        }
+
+        private static void ProcessSpaceKey()
+        {
+
+        }
+
+        private static void ProcessBackspaceKey()
+        {
+
+        }
+
+        private static void ProcessSendKey()
+        {
+
+        }
+
+        private static void PrepareOutputData()
+        {
+
+        }
     }
 }
