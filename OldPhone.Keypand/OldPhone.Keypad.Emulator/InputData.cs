f947cf313af0c0c16fdb52cc6406f101b0d8d39f
a_albrecht <a.albrecht@home-albrecht.de>
2023-05-19 09:12:06 +0200

    data model created

--- OldPhone.Keypand/OldPhone.Keypad.Emulator/InputData.cs
+++ OldPhone.Keypand/OldPhone.Keypad.Emulator/InputData.cs
@@ -1,13 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace OldPhone.Keypad.Emulator
+﻿namespace OldPhone.Keypad.Emulator
 {
+    /// <summary>
+    /// Data model for OldPhone
+    /// </summary>
     public class InputData
     {
+        /// <summary>
+        /// the lase processed input key
+        /// </summary>
+        public string LastKey { get; set; }
 
+        /// <summary>
+        /// the current processed input key
+        /// </summary>
+        public string CurrentKey { get; set; }
+
+        /// <summary>
+        /// the key modifier to identify the parsed value 
+        /// </summary>
+        public int KeyModifier { get; set; }
+
+        /// <summary>
+        /// the original processed input data
+        /// </summary>
+        public string OriginInput { get; set; }
+
+        /// <summary>
+        /// the parsed process input data
+        /// </summary>
+        public string ParsedInput { get; set; }
     }
 }
